e0edc15451642c689a40bf2ef7d7bb5dc18c12d1
Diego <dcavadia@uninorte.edu.co>
2023-05-08 00:10:44 -0500

    add camps contact

--- pages/camps.js
+++ pages/camps.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import CampsHeader from "@/components/camps/CampsHeader";
 import CampsNav from "@/components/camps/CampsNav";
 import CampContent from "@/components/camps/CampContent";
+import Button from "@/components/Button";
+import Footer from "@/components/footer/Footer";
+
+import styles from "../styles/camps/Camps.module.css";
 
 const camps = () => {
   const [selectedOption, setSelectOption] = useState("option1");
@@ -15,6 +19,23 @@ const camps = () => {
       <CampsHeader />
       <CampsNav onSelect={handleOptionSelect} selectedOption={selectedOption} />
       <CampContent selectedOption={selectedOption} />
+      <div className={styles.contactContainer}>
+        <h2 className={styles.contactTitle}>Contact</h2>
+        <p className={styles.contactText}>
+          If you’ve got any questions before booking we’re happy to help. Reach
+          us out at our office hours, Monday to Friday 9:00 am COL to 6:00 pm
+          COL, through email or whatsapp
+        </p>
+        <div className={styles.contactButtonDiv}>
+          <Button textButton={"Send Email"} typeBtn={"second"} url={"#"} />
+          <Button
+            textButton={"Whatsapp"}
+            typeBtn={"second"}
+            url={"https://wa.me/message/KCVNME7YYHBSA1"}
+          />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
