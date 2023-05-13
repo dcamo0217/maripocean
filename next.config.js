c839889d6e636e67a2860c23c9491504dab821e0
Diego <dcavadia@uninorte.edu.co>
2023-05-13 13:44:55 -0500

    fix nextconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  distDir: "build", //this line will tell the build to create a file with this name
+  // output: "export",
+  // distDir: "build", //this line will tell the build to create a file with this name
   reactStrictMode: true,
 };
 
